ed461911745ff6362cf6bd44f57b6f22d9dd7744
bp22013 <118529334+bp22013@users.noreply.github.com>
2024-06-29 01:03:52 +0900

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import React, { ReactNode, Suspense } from 'react';
+import { Inter } from "next/font/google";
+import { NextFont } from 'next/dist/compiled/@next/font';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import Footer from "@/app/component/layout/Footer";
+import "@/app/style/globals.css";
+import { NavigationBar } from "@/app/component/layout/navbar";
+
+const inter: NextFont = Inter({subsets: ['latin']});
+
+export const metadata: Metadata = {
+    title: "Home Page",
+    description: "Masaki's Home Page",
+};
+
+export default async function RootLayout({children}: {children: ReactNode}) {
+    return (
+        <html lang='ja'>
+            <body className={inter.className}>
+                <Analytics />
+                <SpeedInsights />
+                <Suspense>
+                    <div className='flex h-screen flex-col'>
+                        <NavigationBar />
+                        <div className='mb-auto'>{children}</div>
+                        <Footer/>
+                    </div>
+                </Suspense>
+            </body>
+        </html>
+    );
+}

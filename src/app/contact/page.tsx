4003253bd84c33b54f70ca26ebf88002541fcdb0
betamaxbot <bp22013@shibaura-it.ac.jp>
2025-10-05 02:57:43 +0900

    mconfigを修正

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,56 +2,95 @@
 
 import type { NextPage } from 'next';
 import React from 'react';
-import { Button, Input, Textarea, Modal, ModalContent, ModalBody, useDisclosure } from '@nextui-org/react';
+import {
+    Button,
+    Input,
+    Textarea,
+    Modal,
+    ModalContent,
+    ModalBody,
+    useDisclosure,
+} from '@nextui-org/react';
 import { Bounce, toast, ToastContainer } from 'react-toastify';
 
 const Content: NextPage = () => {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      const form = event.currentTarget;
+        event.preventDefault();
+        const form = event.currentTarget;
 
-      toast('お問い合わせ完了しました', {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        transition: Bounce,
-      });
-      
-      setTimeout(() => {
-        form.submit();
-        onclose;
-      }, 3000);
-    }
+        toast('お問い合わせ完了しました', {
+            position: 'top-right',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+            transition: Bounce,
+        });
 
-  return (
-    <div className="max-w-md mx-auto mt-4 p-6 rounded shadow-md">
-      <h1 className="text-xl font-bold mb-8 text-center">お問い合わせフォーム</h1>
-      <form onSubmit={handleSubmit} action={process.env.NEXT_PUBLIC_NEWT_FORM_ENDPOINT} method="post">
-        <div className="mb-4">
-          <label htmlFor="name" className="block text-sm font-medium text-center">お名前</label>
-          <Input type="text" id="name" name="name" required className="mt-1 mb-6 p-2 w-full border rounded-md" />
-          <label htmlFor="email" className="block text-sm font-medium text-center">メールアドレス</label>
-          <Input type="email" id="email" name="email" required className="mt-1 mb-6 p-2 w-full border rounded-md" />
-          <label htmlFor="message" className="block text-sm font-medium text-center">お問い合わせ内容</label>
-          <Textarea id="message" name="message" required className="mt-1 mb-8 p-2 w-full border rounded-md" rows={3}></Textarea>
-          <div className="flex justify-center">
-            <Button type="submit" className="px-8 py-2 rounded bg-blue-600 forground text-white" onPress={onOpen}>
-              送信
-            </Button>
-          </div>
+        setTimeout(() => {
+            form.submit();
+            onclose;
+        }, 3000);
+    };
+
+    return (
+        <div className="max-w-md mx-auto mt-4 p-6 rounded shadow-md">
+            <h1 className="text-xl font-bold mb-8 text-center">お問い合わせフォーム</h1>
+            <form
+                onSubmit={handleSubmit}
+                action={process.env.NEXT_PUBLIC_NEWT_FORM_ENDPOINT}
+                method="post"
+            >
+                <div className="mb-4">
+                    <label htmlFor="name" className="block text-sm font-medium text-center">
+                        お名前
+                    </label>
+                    <Input
+                        type="text"
+                        id="name"
+                        name="name"
+                        required
+                        className="mt-1 mb-6 p-2 w-full border rounded-md"
+                    />
+                    <label htmlFor="email" className="block text-sm font-medium text-center">
+                        メールアドレス
+                    </label>
+                    <Input
+                        type="email"
+                        id="email"
+                        name="email"
+                        required
+                        className="mt-1 mb-6 p-2 w-full border rounded-md"
+                    />
+                    <label htmlFor="message" className="block text-sm font-medium text-center">
+                        お問い合わせ内容
+                    </label>
+                    <Textarea
+                        id="message"
+                        name="message"
+                        required
+                        className="mt-1 mb-8 p-2 w-full border rounded-md"
+                        rows={3}
+                    ></Textarea>
+                    <div className="flex justify-center">
+                        <Button
+                            type="submit"
+                            className="px-8 py-2 rounded bg-blue-600 forground text-white"
+                            onPress={onOpen}
+                        >
+                            送信
+                        </Button>
+                    </div>
+                </div>
+            </form>
+            <ToastContainer />
         </div>
-      </form>
-      <ToastContainer />
-    </div>
-  );
-}
+    );
+};
 
 export default Content;
--- src/utils/Toast/ToastProvider.tsx
+++ src/utils/Toast/ToastProvider.tsx
@@ -1,41 +1,41 @@
-"use client";
+'use client';
 
-import "react-toastify/dist/ReactToastify.css";
-import "@/app/style/globals.css";
-import { Bounce, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import '@/app/style/globals.css';
+import { Bounce, ToastContainer } from 'react-toastify';
 
 interface ToastProviderProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
-export default function ToastProvider({ children }: ToastProviderProps,) {
-  const contextClass = {
-    success: "bg-blue-600",
-    error: "bg-red-600",
-    info: "bg-gray-600",
-    warning: "bg-orange-400",
-    default: "bg-indigo-600",
-    dark: "bg-white-600 font-gray-300",
-  };
+export default function ToastProvider({ children }: ToastProviderProps) {
+    const contextClass = {
+        success: 'bg-blue-600',
+        error: 'bg-red-600',
+        info: 'bg-gray-600',
+        warning: 'bg-orange-400',
+        default: 'bg-indigo-600',
+        dark: 'bg-white-600 font-gray-300',
+    };
 
-  return (
-    <>
-      {children}
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        closeOnClick
-        pauseOnHover
-        draggable
-        theme="light"
-        transition={Bounce}
-        toastClassName={(context) =>
-          contextClass[context?.type || "default"] +
-          " relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"
-        }
-        bodyClassName={() => "text-sm font-white font-med block p-3"}
-      />
-    </>
-  );
+    return (
+        <>
+            {children}
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                closeOnClick
+                pauseOnHover
+                draggable
+                theme="light"
+                transition={Bounce}
+                toastClassName={(context) =>
+                    contextClass[context?.type || 'default'] +
+                    ' relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer'
+                }
+                bodyClassName={() => 'text-sm font-white font-med block p-3'}
+            />
+        </>
+    );
 }
